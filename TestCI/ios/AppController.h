3d46823510695b0a2162cd0c8edfec7eee9ae495
hao-ming <hao-ming@meyougames.com>
2013-11-02 20:38:59 +0800

    Initial Commit

---
+++ TestCI/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  TestCIAppController.h
+//  TestCI
+//
+//  Created by MeYouGames on 13-11-2.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ TestCI/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  TestCIAppController.h
+//  TestCI
+//
+//  Created by MeYouGames on 13-11-2.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
