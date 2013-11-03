b3163bd29d7e23c0b9bb11e4c28bda8fe7a67f4b
hao-ming <hao-ming@meyougames.com>
2013-11-03 21:00:07 +0800

    恢复cocos2d-x logo sprite

--- TestCI/Classes/HelloWorldScene.cpp
+++ TestCI/Classes/HelloWorldScene.cpp
@@ -63,13 +63,13 @@ bool HelloWorld::init()
     this->addChild(pLabel, 1);
 
     // add "HelloWorld" splash screen"
-//    CCSprite* pSprite = CCSprite::create("HelloWorld.png");
-//
-//    // position the sprite on the center of the screen
-//    pSprite->setPosition( ccp(0, size.height/2) );
-//
-//    // add the sprite as a child to this layer
-//    this->addChild(pSprite, 0);
+    CCSprite* pSprite = CCSprite::create("HelloWorld.png");
+
+    // position the sprite on the center of the screen
+    pSprite->setPosition( ccp(0, size.height/2) );
+
+    // add the sprite as a child to this layer
+    this->addChild(pSprite, 0);
     
     return true;
 }
